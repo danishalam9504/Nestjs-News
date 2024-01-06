cb8409a2ea6f3c63b6491e2139aa2295b233d02b
danish9504 <danish.alam@dazn.com>
2024-01-07 00:22:32 +0530

    add env file

--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -2,16 +2,18 @@ import { Module } from '@nestjs/common';
 import { NewsController } from './news.controller';
 import { NewsService } from './news.service';
 import { ElasticsearchModule } from '@nestjs/elasticsearch';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
-  imports:[
+  imports: [
     ElasticsearchModule.registerAsync({
-      useFactory: () => ({
-        node: 'https://6410070b697146d6972cc1af12c0d881.us-central1.gcp.cloud.es.io:443',
+      imports: [ConfigModule], // Import the ConfigModule
+      useFactory: async (configService: ConfigService) => ({
+        node: configService.get<string>('ELASTICSEARCH_NODE'),
         auth: {
-          apiKey: 'QUVySjFZd0JCa3F4RnpEVWIycGE6NlRrMlRVTW9SejZmUzRtY3UxSHEwZw==',
+          apiKey: configService.get<string>('ELASTICSEARCH_API_KEY'),
         },
       }),
+      inject: [ConfigService], // Inject ConfigService into useFactory
     }),
   ],
   controllers: [NewsController],
