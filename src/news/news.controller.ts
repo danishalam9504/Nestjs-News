c7e6b59072a3a2a1a23582489f64596eabecf568
danish9504 <danish.alam@dazn.com>
2024-02-17 12:09:54 +0530

    accept the data from formdata and json

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,7 +1,11 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, Req, UploadedFiles, UseInterceptors, UsePipes, ValidationPipe } from '@nestjs/common';
 import { NewsService } from './news.service';
 import { NewsDto } from './dto/news.dto';
 import { SearchDto } from './dto/search.dto';
+import { truncate } from 'fs';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { Request } from 'express';
+
 
 @Controller('news')
 export class NewsController {
@@ -27,76 +31,89 @@ export class NewsController {
   }
 
   @Post('search')
-  @UsePipes(new ValidationPipe({ whitelist: true }))
-  async searchByKey(@Body() data: SearchDto) {
-    const { search_value, search_key, sort_by, sort_by_key, from, size, country, category, creator, language, article_source } = data;
-
-    let filter = [];
-    let must: any;
-
-    // Check if search_value is null, undefined, or an empty string
-    if (!search_value || search_value.trim() === "") {
-      must = {
-        "match_all": {}
-      };
-    } else {
-      must = {
-        "multi_match": {
-          "query": search_value,
-          "fields": search_key
-        }
-      };
+  // @UsePipes(new ValidationPipe({ whitelist: true }))
+  @UseInterceptors(FilesInterceptor('files'))
+  async searchByKey(@Req() req: Request) {
+    const contentType = req.headers['content-type'];
+    const formData = req.body; // This will contain the form data fields
+    let { search_value, search_key, sort_by, sort_by_key, country, category, creator, language, article_source, from, size } = formData;
+    if (contentType.includes('multipart/form-data')) {
+      search_key = search_key.split(',');
+      country = country.split(',');
+      category = category.split(','); article_source
+      creator = creator.split(',');
+      language = language.split(',');
+      article_source = article_source.split(',');
+      from = +from;
+      size = +size;
     }
 
-    // Function to add terms to filter if array is not empty
-    const addTermsToFilter = async (field: string, values: any) => {
-      if (values && values.length > 0) {
-        filter.push({
-          terms: {
-            [`${field}.keyword`]: values
+      let filter = [];
+      let must: any;
+
+      // Check if search_value is null, undefined, or an empty string
+      if (!search_value || search_value.trim() === "") {
+        must = {
+          "match_all": {}
+        };
+      } else {
+        must = {
+          "multi_match": {
+            "query": search_value,
+            "fields": search_key
           }
-        });
+        };
       }
-    };
 
-    // Add terms to filter based on non-empty arrays
-    addTermsToFilter('country', country);
-    addTermsToFilter('category', category);
-    addTermsToFilter('creator', creator);
-    addTermsToFilter('language', language);
-    addTermsToFilter('article_source', article_source);
-
-    this.query = {
-      "query": {
-        "bool": {
-          "must": must,
-          "filter": filter
+      // Function to add terms to filter if array is not empty
+      const addTermsToFilter = async (field: string, values: any) => {
+        if (values && values.length > 0) {
+          filter.push({
+            terms: {
+              [`${field}.keyword`]: values
+            }
+          });
         }
-      },
-      "sort": [
-        {
-          [sort_by_key]: {
-            "order": sort_by
+      };
+
+      // Add terms to filter based on non-empty arrays
+      addTermsToFilter('country', country);
+      addTermsToFilter('category', category);
+      addTermsToFilter('creator', creator);
+      addTermsToFilter('language', language);
+      addTermsToFilter('article_source', article_source);
+
+      this.query = {
+        "query": {
+          "bool": {
+            "must": must,
+            "filter": filter
           }
-        }
-      ],
-      "from": from,
-      "size": size
-    }
-    const EsData = await this.newsService.executeQuery(this.index, this.query);
-    const extractedData = this.newsService.extractRequiredFields(EsData);
-    const count = EsData['hits']['total']['value'];
+        },
+        "sort": [
+          {
+            [sort_by_key]: {
+              "order": sort_by
+            }
+          }
+        ],
+        "from": from,
+        "size": size
+      }
+      const EsData = await this.newsService.executeQuery(this.index, this.query);
+      const extractedData = this.newsService.extractRequiredFields(EsData);
+      const count = EsData['hits']['hits'].length;
 
-    return {
-      status: "ok",
-      data: extractedData,
-      size: count
-    };
+      return {
+        status: "ok",
+        data: extractedData,
+        size: count
+      };
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-  
+
     this.query = this.newsService.queryForSingleNewsValue(id);
     const EsData = await this.newsService.executeQuery(this.index, this.query);
     const extractedData = this.newsService.extractRequiredFields(EsData);
